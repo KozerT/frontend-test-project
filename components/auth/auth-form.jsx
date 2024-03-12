349b2589431b2fbe37628de8f52c848627526590
KozerT <kozer.vizastaff@gmail.com>
2024-03-12 15:07:10 +0100

    feat(auth): implement authorization, enhance Auth Form with local storage, and style Home page

--- components/auth/auth-form.jsx
+++ components/auth/auth-form.jsx
@@ -1,59 +1,111 @@
+"use client";
 import Link from "next/link";
 import styles from "./auth-form.module.css";
 import FormAuthButton from "./form-auth-button";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AuthForm() {
+  const router = useRouter();
+  const [emailOrUsername, setEmailOrUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    const savedEmail = localStorage.getItem("email");
+    const savedPassword = localStorage.getItem("password");
+
+    if (savedEmail && savedPassword) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setErrors({});
+
+    if (emailOrUsername.toLowerCase() === "liam" && password === "123123") {
+      setIsLoggedIn(true);
+      localStorage.setItem("email", emailOrUsername);
+      localStorage.setItem("password", password);
+      router.push("/posts");
+    } else {
+      const newErrors = {};
+
+      if (password !== "123123") {
+        newErrors.password = "Invalid password";
+      }
+
+      if (!(emailOrUsername.toLowerCase() === "liam")) {
+        newErrors.emailOrUsername = "Invalid username or email";
+      }
+
+      setErrors(newErrors);
+    }
+  };
+
   return (
-    <form className={styles.form}>
-      <div className={styles["input-group"]}>
-        <label htmlFor="email" className={styles["form-label"]}>
-          Username or Email
-        </label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          autoComplete="email"
-          required
-          className={styles["form-input"]}
-        />
-      </div>
-      <div className={styles["input-group"]}>
-        <div className={styles["password-group"]}>
-          <label htmlFor="password" className={styles["form-label"]}>
-            Password
+    <>
+      <form className={styles.form} onSubmit={onFormSubmit}>
+        <div className={styles["input-group"]}>
+          <label htmlFor="emailOrUsername" className={styles["form-label"]}>
+            Username or Email
           </label>
-          <Link
-            href="/password-recovery"
-            className={styles["password-recovery-link"]}
-          >
-            Forgot?
-          </Link>
+          <input
+            id="emailOrUsername"
+            type="text"
+            name="emailOrUsername"
+            autoComplete="off"
+            value={emailOrUsername}
+            onChange={(event) => setEmailOrUsername(event.target.value)}
+            required
+            className={`${styles["form-input"]} ${
+              errors.emailOrUsername ? styles.errorInput : ""
+            }`}
+          />
+          {errors.emailOrUsername && (
+            <p className={styles.error}>{errors.emailOrUsername}</p>
+          )}
         </div>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          autoComplete="current-password"
-          required
-          className={styles["form-input"]}
-        />
-      </div>
-      <Link
-        href="/posts"
-        passHref={true}
-        className={styles["auth-button-link"]}
-      >
-        <FormAuthButton />
-      </Link>
-      <div className={styles["form-signup-container"]}>
-        <p>Don &apos;t have an account?</p>
-        <span>
-          <Link href="/signup" className={styles["form-signup-link"]}>
-            Sign up
-          </Link>
-        </span>
-      </div>
-    </form>
+        <div className={styles["input-group"]}>
+          <div className={styles["password-group"]}>
+            <label htmlFor="password" className={styles["form-label"]}>
+              Password
+            </label>
+            <Link
+              href="/password-recovery"
+              className={styles["password-recovery-link"]}
+            >
+              Forgot?
+            </Link>
+          </div>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+            autoComplete="current-password"
+            onChange={(event) => setPassword(event.target.value)}
+            required
+            className={`${styles["form-input"]} ${
+              errors.password ? styles.errorInput : ""
+            }`}
+          />
+          {errors.password && <p className={styles.error}>{errors.password}</p>}
+        </div>
+        <div className={styles["auth-button-link"]}>
+          <FormAuthButton disabled={!isLoggedIn} />
+        </div>
+        <div className={styles["form-signup-container"]}>
+          <p>Don&apos;t have an account?</p>
+          <span>
+            <Link href="/signup" className={styles["form-signup-link"]}>
+              Sign up
+            </Link>
+          </span>
+        </div>
+      </form>
+    </>
   );
 }
--- components/auth/form-auth-button.jsx
+++ components/auth/form-auth-button.jsx
@@ -1,9 +1,5 @@
-"use client";
-import { useState } from "react";
 import styles from "./form-auth-button.module.css";
 
 export default function FormAuthButton() {
-  const [isLogin, setIsLogin] = useState();
-
   return <button className={styles.button}>Sign In</button>;
 }
